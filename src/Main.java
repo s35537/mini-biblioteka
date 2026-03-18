70b0bc498641ccf7f1ed2310f157448a03e45932
Kajetan Możdżeń <s35537@pjwstk.edu.pl>
2026-03-18 09:37:55 +0100

    Dodanie metod pomocniczych

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         Ksiazka k1 = new Ksiazka("Wiedzmin", "Sapkowski", 332, true);
-        Ksiazka k2 = new Ksiazka("Silos", "Howey", 624, true);
+        Ksiazka k2 = new Ksiazka("Solaris", "Lem", 204, true);
         Ksiazka k3 = new Ksiazka("1984", "Orwell", 328, true);
 
         k1.wypiszInfo();
-        k1.wypozycz();
-        k1.wypiszInfo();
-        k1.zwroc();
-        k1.wypiszInfo();
-
         k2.wypiszInfo();
         k3.wypiszInfo();
+
+        Czytelnik c1 = new Czytelnik("Paweł", "Nowakowski", 1001);
+        Czytelnik c2 = new Czytelnik("Kasia", "Kowalska", 1002);
+
+        c1.wypiszDane();
+        c1.zwiekszLiczbeWypozyczen();
+        c1.wypiszDane();
+        c2.wypiszDane();
     }
 }
