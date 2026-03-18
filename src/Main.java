bed0b7411f19bdbe362155d8f1c212b3a976de38
Kajetan Możdżeń <s35537@pjwstk.edu.pl>
2026-03-18 09:48:44 +0100

    Dodanie scenariusza testowego aplikacji

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        Biblioteka bib = new Biblioteka(10);
+
         Ksiazka k1 = new Ksiazka("Wiedzmin", "Sapkowski", 332, true);
-        Ksiazka k2 = new Ksiazka("Solaris", "Lem", 204, true);
+        Ksiazka k2 = new Ksiazka("Silos", "Howey", 624, true);
         Ksiazka k3 = new Ksiazka("1984", "Orwell", 328, true);
 
-        k1.wypiszInfo();
-        k2.wypiszInfo();
-        k3.wypiszInfo();
+        bib.dodajKsiazke(k1);
+        bib.dodajKsiazke(k2);
+        bib.dodajKsiazke(k3);
 
-        Czytelnik c1 = new Czytelnik("Paweł", "Nowakowski", 1001);
-        Czytelnik c2 = new Czytelnik("Kasia", "Kowalska", 1002);
+        Czytelnik c1 = new Czytelnik("Pawel", "Kowalski", 1001);
 
+        System.out.println("--- Dostepne przed wypozyczeniem ---");
+        bib.wypiszDostepneKsiazki();
+
+        bib.wypozyczKsiazke("Silos", c1);
         c1.wypiszDane();
-        c1.zwiekszLiczbeWypozyczen();
-        c1.wypiszDane();
-        c2.wypiszDane();
+
+        System.out.println("--- Dostepne po wypozyczeniu ---");
+        bib.wypiszDostepneKsiazki();
+
+        bib.zwrocKsiazke("Silos", c1);
+
+        System.out.println("--- Dostepne po zwrocie ---");
+        bib.wypiszDostepneKsiazki();
+
+        System.out.println("Liczba dostepnych: " + bib.policzDostepneKsiazki());
     }
 }
