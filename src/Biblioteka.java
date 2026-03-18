b49ea1da4d83a9aa81dc078caa0f07054e9b64f0
Kajetan Możdżeń <s35537@pjwstk.edu.pl>
2026-03-18 09:41:41 +0100

    Dodanie klasy Biblioteka z tablicą książek

---
+++ src/Biblioteka.java
@@ -0,0 +1,44 @@
+public class Biblioteka {
+    private Ksiazka[] ksiazki;
+    private int liczbaKsiazek;
+
+    public Biblioteka(int pojemnosc) {
+        ksiazki = new Ksiazka[pojemnosc];
+        liczbaKsiazek = 0;
+    }
+
+    public void dodajKsiazke(Ksiazka ksiazka) {
+        if (liczbaKsiazek < ksiazki.length) {
+            ksiazki[liczbaKsiazek] = ksiazka;
+            liczbaKsiazek++;
+        } else {
+            System.out.println("Biblioteka jest pelna!");
+        }
+    }
+
+    public void wypiszDostepneKsiazki() {
+        System.out.println("Dostepne ksiazki:");
+        for (int i = 0; i < liczbaKsiazek; i++) {
+            if (ksiazki[i].isDostepna()) {
+                ksiazki[i].wypiszInfo();
+            }
+        }
+    }
+
+    public Ksiazka znajdzKsiazkePoTytule(String tytul) {
+        for (int i = 0; i < liczbaKsiazek; i++) {
+            if (ksiazki[i].getTytul().equalsIgnoreCase(tytul)) {
+                return ksiazki[i];
+            }
+        }
+        return null;
+    }
+
+    public int policzDostepneKsiazki() {
+        int licznik = 0;
+        for (int i = 0; i < liczbaKsiazek; i++) {
+            if (ksiazki[i].isDostepna()) licznik++;
+        }
+        return licznik;
+    }
+}
